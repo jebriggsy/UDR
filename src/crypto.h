009afbe097da5e403e0dca327176b8f3c0416900
Dominique Martinet <asmadeus@codewreck.org>
2018-05-20 09:04:32 +0900

    crypto: update EVP_CIPHER_CTX api for openssl 1.1+
    Fixes #39.

--- src/crypto.h
+++ src/crypto.h
@@ -47,7 +47,7 @@ class crypto
     int hex_passphrase_size;
 
     // EVP stuff
-    EVP_CIPHER_CTX ctx;
+    EVP_CIPHER_CTX *ctx;
 
     public:
 
@@ -106,20 +106,18 @@ class crypto
 
         direction = direc;
         // EVP stuff
-        EVP_CIPHER_CTX_init(&ctx);
+        ctx = EVP_CIPHER_CTX_new();
 
-        if (!EVP_CipherInit_ex(&ctx, cipher, NULL, password, ivec, direc)) {
+        if (!EVP_CipherInit_ex(ctx, cipher, NULL, password, ivec, direc)) {
             fprintf(stderr, "error setting encryption scheme\n");
             exit(EXIT_FAILURE);
         }
     }
 
-//    ~crypto()
-//    {
-//        // i guess thread issues break this but it needs to be done
-//        //TODO: find out why this is bad and breaks things
-//        EVP_CIPHER_CTX_cleanup(&ctx);
-//    }
+    ~crypto()
+    {
+        EVP_CIPHER_CTX_free(ctx);
+    }
 
     // Returns how much has been encrypted and will call encrypt final when
     // given len of 0
@@ -128,14 +126,14 @@ class crypto
         int evp_outlen;
 
         if (len == 0) {
-            if (!EVP_CipherFinal_ex(&ctx, (unsigned char *)out, &evp_outlen)) {
+            if (!EVP_CipherFinal_ex(ctx, (unsigned char *)out, &evp_outlen)) {
                 fprintf(stderr, "encryption error\n");
                 exit(EXIT_FAILURE);
             }
             return evp_outlen;
         }
 
-        if(!EVP_CipherUpdate(&ctx, (unsigned char *)out, &evp_outlen, (unsigned char *)in, len))
+        if(!EVP_CipherUpdate(ctx, (unsigned char *)out, &evp_outlen, (unsigned char *)in, len))
         {
             fprintf(stderr, "encryption error\n");
             exit(EXIT_FAILURE);
