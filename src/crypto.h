ac8790e8b636621b3f35c7a943c673323cbb6b98
Kristján Valur Jónsson <sweskman@gmail.com>
2020-05-20 20:14:40 +0000

    Make crypto work with openssl 1.1.0 and later

--- src/crypto.h
+++ src/crypto.h
@@ -24,6 +24,8 @@ and limitations under the License.
 #define EVP_DECRYPT 0
 #define CTR_MODE 1
 
+#include "udr_options.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -40,19 +42,23 @@ class crypto
 {
     private:
     //BF_KEY key;
-    unsigned char ivec[ 1024 ];
     int direction;
 
     int passphrase_size;
     int hex_passphrase_size;
 
     // EVP stuff
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
     EVP_CIPHER_CTX ctx;
+#endif
+    EVP_CIPHER_CTX *ctxp;
 
     public:
 
     crypto(int direc, int len, unsigned char* password, char *encryption_type)
     {
+        unsigned char ivec[EVP_MAX_IV_LENGTH];
+        unsigned char keybuf[EVP_MAX_KEY_LENGTH];
         //free_key( password ); can't free here because is reused by threads
         const EVP_CIPHER *cipher;
 
@@ -102,42 +108,48 @@ class crypto
             exit(EXIT_FAILURE);
         }
 
-        memset(ivec, 0, 1024);
+        // password is reset for each session, can use 0 IV
+        memset(ivec, 0, sizeof(ivec));
+        memset(keybuf, 0, sizeof(keybuf));
+        memcpy(keybuf, password, (size_t)len > sizeof(keybuf) ? sizeof(keybuf) : (size_t)len);
 
         direction = direc;
         // EVP stuff
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
         EVP_CIPHER_CTX_init(&ctx);
+        ctxp = &ctx;
+#else
+        ctxp = EVP_CIPHER_CTX_new();
+#endif
 
-        if (!EVP_CipherInit_ex(&ctx, cipher, NULL, password, ivec, direc)) {
-            fprintf(stderr, "error setting encryption scheme\n");
+        if (!EVP_CipherInit_ex(ctxp, cipher, NULL, keybuf, ivec, direc)) {
+            goptions.err() << "error setting encryption scheme" << endl;
             exit(EXIT_FAILURE);
         }
     }
 
-//    ~crypto()
-//    {
-//        // i guess thread issues break this but it needs to be done
-//        //TODO: find out why this is bad and breaks things
-//        EVP_CIPHER_CTX_cleanup(&ctx);
-//    }
+    ~crypto()
+    {
+        // threads must be dead by now
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+        EVP_CIPHER_CTX_cleanup(&ctx);
+#else
+        EVP_CIPHER_CTX_free(ctxp);
+#endif
+    }
 
-    // Returns how much has been encrypted and will call encrypt final when
-    // given len of 0
+    // using a stream mode causes us to always get the same size of
+    // data in and out.  Also, no EVP_CipherFinal_ex() is needed
+    //
+
     int encrypt(char *in, char *out, int len)
     {
         int evp_outlen;
 
-        if (len == 0) {
-            if (!EVP_CipherFinal_ex(&ctx, (unsigned char *)out, &evp_outlen)) {
-                fprintf(stderr, "encryption error\n");
-                exit(EXIT_FAILURE);
-            }
-            return evp_outlen;
-        }
-
-        if(!EVP_CipherUpdate(&ctx, (unsigned char *)out, &evp_outlen, (unsigned char *)in, len))
+        if(!EVP_CipherUpdate(ctxp, (unsigned char *)out, &evp_outlen, (unsigned char *)in, len))
         {
             fprintf(stderr, "encryption error\n");
+            goptions.err() << "encryption error" << endl;
             exit(EXIT_FAILURE);
         }
         return evp_outlen;
